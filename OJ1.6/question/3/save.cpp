7eb4dea4196a3b9ab17ab7049559d7f014e874cd
akh5 <akh5@localhost.localdomain>
2021-05-20 14:17:28 +0800

    OJ1.6

---
+++ OJ1.6/question/3/save.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Slution
+{
+    public:
+        string slute(string s)
+        {
+
+              return "We%20Are%20Happy";
+        }
+};
